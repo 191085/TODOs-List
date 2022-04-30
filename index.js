e04842754ffc4c835fbb6a5e1f8492776e6f15fe
Tech Fast Solution <92201619+191085@users.noreply.github.com>
2022-04-30 14:06:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+    
+        // 1. ways to print javascript
+        //  console.log("hello world ");
+        // alert("this is me ");
+        // document.write("this is document write")
+
+        //2. Java script console API
+
+        console.log("hello world ",4+7,"rajmal");
+        console.warn("this is warning");
+        console.error("this is an error");
+        // console.clear("this is clear");
+
